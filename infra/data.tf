ec1c7e3b1044a5415f5c9b1e68f053c25fc1df3d
Valdir Luiz <arnholdvaldir@gmail.com>
2025-07-12 14:06:03 -0300

    feat: alterar api

--- infra/data.tf
+++ infra/data.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 data "template_file" "openapi" {
-  template = file("${path.module}/open-api/api.yml")
+  template = file("${path.module}/open-api/open-api-v1.yml")
   vars = {
     region     = local.region
     account_id = local.account_id
